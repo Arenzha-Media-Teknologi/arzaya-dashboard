81b086d01e9388251a928364b74d953a4bdde42d
royhanreza <royhanreza842@gmail.com>
2023-06-06 10:52:17 +0700

    feat: add leaderboard page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import App from "./App";
 // import "./index.css";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import JobList from "./routes/JobList";
+import "@fontsource-variable/inter";
+import Scoring from "./routes/Scoring";
 
 // 2. Add your color mode config
 const config = {
@@ -16,6 +20,19 @@ const customTheme = extendTheme({ config });
 
 const queryClient = new QueryClient();
 
+// routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <JobList />,
+    // errorElement: <ErrorPage />,
+  },
+  {
+    path: "/scoring",
+    element: <Scoring />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
@@ -25,9 +42,13 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             initialColorMode: "dark",
             useSystemColorMode: false,
           },
+          fonts: {
+            inter: `'Inter Variable', sans-serif`,
+          },
         })}
       >
-        <App />
+        {/* <App /> */}
+        <RouterProvider router={router} />
       </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>
